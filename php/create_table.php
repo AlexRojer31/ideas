970551f710a2e781c53bc7872df2dae4598d9a4f
AlexRojer31 <75638092+AlexRojer31@users.noreply.github.com>
2021-01-16 19:19:36 +0300

    Update create_table.php

--- php/create_table.php
+++ php/create_table.php
@@ -6,6 +6,6 @@ $db_require = $_POST["db_require"];
 $result_db_require = mysqli_query($link, $db_require)
 	or die("Error".mysqli_error($link));
 
-echo "Request good <a href=\"adminka\">back</a>";
+echo "Request good <a href=\"phpMyAdmin\">back</a>";
 
 ?>
