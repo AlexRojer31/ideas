683e8a47d492095284758750488f9b16afb39e0a
AlexRojer31 <75638092+AlexRojer31@users.noreply.github.com>
2021-01-16 19:18:59 +0300

    Create create_table.php

---
+++ php/create_table.php
@@ -0,0 +1,11 @@
+<?php
+require_once "database_connect.php";
+
+$db_require = $_POST["db_require"];
+
+$result_db_require = mysqli_query($link, $db_require)
+	or die("Error".mysqli_error($link));
+
+echo "Request good <a href=\"adminka\">back</a>";
+
+?>
